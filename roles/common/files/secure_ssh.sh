49dc397a66699a46534c46054f9b54d8f1ab697e
Haitao Pan <manbuzhe2009@qq.com>
2023-12-18 18:02:59 +0800

    roles/clickhouse/tasks/main.yml: update get clickhouse node ip

--- roles/common/files/secure_ssh.sh
+++ roles/common/files/secure_ssh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 设置 ~/.ssh/ 目录的权限
-chmod 700 ~/.ssh
+sudo chmod 700 ~/.ssh
 
 # 设置 ~/.ssh/authorized_keys 文件的权限
-chmod 600 ~/.ssh/authorized_keys
+sudo chmod 600 ~/.ssh/authorized_keys
 
 # 使用 chattr +i 确保 authorized_keys 文件不能被删除
-chattr +i ~/.ssh/authorized_keys || true
+sudo chattr +i ~/.ssh/authorized_keys || true
 
