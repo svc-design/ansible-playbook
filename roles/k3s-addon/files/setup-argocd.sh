cd8641786166fe8d42cf7fc0fc996ae61cba22f3
Haitao Pan <manbuzhe2009@qq.com>
2024-03-11 19:00:47 +0800

    roles/k3s-addon: update setup-argocd

--- roles/k3s-addon/files/setup-argocd.sh
+++ roles/k3s-addon/files/setup-argocd.sh
@@ -8,12 +8,6 @@ check_not_empty() {
   fi
 }
 
-# 检查参数是否为空
-check_not_empty "$1" "Git repository URL" && git_repo=$1
-check_not_empty "$2" "Helmfile name" && helmfile_name=$2
-
-#!/bin/bash
-
 # 添加 Argo CD 的 Helm 仓库
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
@@ -26,30 +20,30 @@ echo "Waiting for Argo CD to be ready..."
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=180s
 
 # 创建 Argo CD Application 配置文件
-cat <<EOF > argocd-application.yaml
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: helmfile-application
-  namespace: argocd
-spec:
-  project: default
-  source:
-    repoURL: $git_repo
-    path: $helmfile_name
-    targetRevision: HEAD
-    helm:
-      releaseName: helmfile-application
-  destination:
-    server: https://kubernetes.default.svc
-    namespace: default
-  syncPolicy:
-    automated:
-      prune: true
-      selfHeal: true
-EOF
-
-# 应用 Argo CD Application 配置
-kubectl apply -f argocd-application.yaml
+#cat <<EOF > argocd-application.yaml
+#apiVersion: argoproj.io/v1alpha1
+#kind: Application
+#metadata:
+#  name: helmfile-application
+#  namespace: argocd
+#spec:
+#  project: default
+#  source:
+#    repoURL: $git_repo
+#    path: $helmfile_name
+#    targetRevision: HEAD
+#    helm:
+#      releaseName: helmfile-application
+#  destination:
+#    server: https://kubernetes.default.svc
+#    namespace: default
+#  syncPolicy:
+#    automated:
+#      prune: true
+#      selfHeal: true
+#EOF
+#
+## 应用 Argo CD Application 配置
+#kubectl apply -f argocd-application.yaml
 
 echo "Argo CD deployment and configuration complete."
