c135b501c34e71798976bd67f6da78be9d144f16
Haitao Pan <manbuzhe2009@qq.com>
2024-03-20 22:13:34 +0800

    k3s-addon/files/setup-argocd.sh: update

--- roles/k3s-addon/files/setup-argocd.sh
+++ roles/k3s-addon/files/setup-argocd.sh
@@ -34,6 +34,63 @@ server:
       nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
       nginx.ingress.kubernetes.io/backend-protocol: "HTTP"
     tls: true
+repoServer:
+  extraContainers:
+    - name: helmfile
+      image: ghcr.io/helmfile/helmfile:v0.157.0
+      # Entrypoint should be Argo CD lightweight CMP server i.e. argocd-cmp-server
+      command: ["/var/run/argocd/argocd-cmp-server"]
+      env:
+        - name: HELM_CACHE_HOME
+          value: /tmp/helm/cache
+        - name: HELM_CONFIG_HOME
+          value: /tmp/helm/config
+        - name: HELMFILE_CACHE_HOME
+          value: /tmp/helmfile/cache
+        - name: HELMFILE_TEMPDIR
+          value: /tmp/helmfile/tmp
+      securityContext:
+        runAsNonRoot: true
+        runAsUser: 999
+      volumeMounts:
+        - mountPath: /var/run/argocd
+          name: var-files
+        - mountPath: /home/argocd/cmp-server/plugins
+          name: plugins
+        # Register helmfile plugin into sidecar
+        - mountPath: /home/argocd/cmp-server/config/plugin.yaml
+          subPath: helmfile.yaml
+          name: argocd-cmp-cm
+        # Starting with v2.4, do NOT mount the same tmp volume as the repo-server container. The filesystem separation helps mitigate path traversal attacks.
+        - mountPath: /tmp
+          name: helmfile-tmp
+  volumes:
+    - name: argocd-cmp-cm
+      configMap:
+        name: argocd-cmp-cm
+    - name: helmfile-tmp
+      emptyDir: {}
+configs:
+  cmp:
+    create: true
+    plugins:
+      helmfile:
+        allowConcurrency: true
+        discover:
+          fileName: helmfile.yaml
+        generate:
+          command:
+            - bash
+            - "-c"
+            - |
+              if [[ -v ENV_NAME ]]; then
+                helmfile -n "$ARGOCD_APP_NAMESPACE" -e $ENV_NAME template --include-crds -q
+              elif [[ -v ARGOCD_ENV_ENV_NAME ]]; then
+                helmfile -n "$ARGOCD_APP_NAMESPACE" -e "$ARGOCD_ENV_ENV_NAME" template --include-crds -q
+              else
+                helmfile -n "$ARGOCD_APP_NAMESPACE" template --include-crds -q
+              fi
+        lockRepo: false
 EOF
 
 helm upgrade --install argocd argo/argo-cd -n argocd -f values.yaml
@@ -41,32 +98,5 @@ helm upgrade --install argocd argo/argo-cd -n argocd -f values.yaml
 # 等待 Argo CD 完全启动
 echo "Waiting for Argo CD to be ready..."
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=180s
-
-# 创建 Argo CD Application 配置文件
-#cat <<EOF > argocd-application.yaml
-#apiVersion: argoproj.io/v1alpha1
-#kind: Application
-#metadata:
-#  name: helmfile-application
-#  namespace: argocd
-#spec:
-  project: default
-#  source:
-#    repoURL: $git_repo
-#    path: $helmfile_name
-#    targetRevision: HEAD
-#    helm:
-#      releaseName: helmfile-application
-#  destination:
-#    server: https://kubernetes.default.svc
-#    namespace: default
-#  syncPolicy:
-#    automated:
-#      prune: true
-#      selfHeal: true
-#EOF
-#
-## 应用 Argo CD Application 配置
-#kubectl apply -f argocd-application.yaml
 
 echo "Argo CD deployment and configuration complete."
