29387091462d361f0ac4b1d59db5e6f80ce97cf3
Haitao Pan <manbuzhe2009@qq.com>
2024-03-23 22:52:30 +0800

    jenkins/files/setup.sh: add docker-plugin

--- roles/jenkins/files/setup.sh
+++ roles/jenkins/files/setup.sh
@@ -30,22 +30,23 @@ controller:
   installLatestPlugins: true
   installPlugins:
     - git:5.2.1
-    - database-mysql:1.4
     - github:1.38.0
     - github-pullrequest:0.7.0
-    - gitlab-plugin:1.7.16
-    - pipeline-stage-view:2.33
-    - database:191.vd5981b_97a_5fa_
     - locale:314.v22ce953dfe9e
-    - kubernetes:4029.v5712230ccb_f8
-    - workflow-job:1385.vb_58b_86ea_fff1  # 更新版本以满足依赖关系
-    - workflow-aggregator:596.v8c21c963d92d
+    - database-mysql:1.4
+    - database:191.vd5981b_97a_5fa_
     - credentials:1337.v60b_d7b_c7b_c9f
     - credentials-binding:642.v737c34dea_6c2  # 更新版本以满足依赖关系
     - configuration-as-code:1775.v810dc950b_514  # 更新版本以满足依赖关系
-    - docker-commons:439.va_3cb_0a_6a_fb_29
+    - gitlab-plugin:1.7.16
+    - kubernetes:4029.v5712230ccb_f8
+    - docker-plugin:1.6
     - docker-workflow:572.v950f58993843
+    - docker-commons:439.va_3cb_0a_6a_fb_29
+    - pipeline-stage-view:2.33
+    - workflow-job:1385.vb_58b_86ea_fff1
     - workflow-cps:3883.vb_3ff2a_e3eea_f
+    - workflow-aggregator:596.v8c21c963d92d
   JCasC:
     enabled: true
     defaultConfig: true
