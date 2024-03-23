be4faa7a3b4779a0c729288936a54491756e324a
Haitao Pan <manbuzhe2009@qq.com>
2024-03-23 13:47:52 +0800

    roles/jenkins/files/setup.sh: update

--- roles/jenkins/files/setup.sh
+++ roles/jenkins/files/setup.sh
@@ -35,12 +35,12 @@ controller:
     - database:191.vd5981b_97a_5fa_
     - locale:314.v22ce953dfe9e
     - kubernetes:4029.v5712230ccb_f8
-    - workflow-job:1385.vb_58b_86ea_fff1  # Updated to resolve dependency conflicts
+    - workflow-job:1385.vb_58b_86ea_fff1  # 更新版本以满足依赖关系
     - workflow-aggregator:596.v8c21c963d92d
-    - credentials-binding:642.v737c34dea_6c2  # Updated to resolve dependency conflicts
-    - configuration-as-code:1775.v810dc950b_514  # Updated to resolve dependency conflicts
+    - credentials-binding:642.v737c34dea_6c2  # 更新版本以满足依赖关系
+    - configuration-as-code:1775.v810dc950b_514  # 更新版本以满足依赖关系
     - docker-workflow:1.26
-    - workflow-cps:3769.v8b_e595e4d40d  # Updated to resolve dependency conflicts
+    - workflow-cps:3883.vb_3ff2a_e3eea_f
   JCasC:
     enabled: true
     defaultConfig: true
