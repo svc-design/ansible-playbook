4ba957bb355763ac8574acabdc5056475ec907de
Haitao Pan <manbuzhe2009@qq.com>
2024-04-06 07:49:11 +0800

    update

--- roles/deepflow/files/setup.sh
+++ roles/deepflow/files/setup.sh
@@ -27,5 +27,5 @@ grafana:
 EOF
 helm repo add deepflow https://deepflowio.github.io/deepflow
 helm repo update deepflow # use `helm repo update` when helm < 3.7.0
-#helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.4.9 -f values-custom.yaml
-helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.2.6 -f values-custom.yaml
+helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.4.9 -f values-custom.yaml
+#helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.2.6 -f values-custom.yaml
