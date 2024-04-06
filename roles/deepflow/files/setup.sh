070100452796212feff6fd96dca59f3478f11b55
Haitao Pan <manbuzhe2009@qq.com>
2024-04-06 09:41:00 +0800

    update

--- roles/deepflow/files/setup.sh
+++ roles/deepflow/files/setup.sh
@@ -5,16 +5,15 @@ export secret=$2
 export namespace=$3
 
 cat << EOF > values-custom.yaml
+global:
+  allInOneLocalStorage: true
 clickhouse:
   enabled: true
 server:
   enabled: true
-  service:
-    type: ClusterIP
 grafana:
   enabled: true
   service:
-    type: ClusterIP
   ingress:
     enabled: true
     ingressClassName: nginx
@@ -28,4 +27,5 @@ EOF
 helm repo add deepflow https://deepflowio.github.io/deepflow
 helm repo update deepflow # use `helm repo update` when helm < 3.7.0
 helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.4.9 -f values-custom.yaml
+
 #helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.2.6 -f values-custom.yaml
