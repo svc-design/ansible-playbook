2aa024387a7386512b3fcb307d629d92f899d507
Haitao Pan <manbuzhe2009@qq.com>
2024-04-06 14:56:25 +0800

    update

--- roles/deepflow/files/setup.sh
+++ roles/deepflow/files/setup.sh
@@ -11,12 +11,8 @@ clickhouse:
   enabled: true
 server:
   enabled: true
-  dnsPolicy: ClusterFirstWithHostNet
 deepflow-agent:
   enabled: true
-  deepflowServerNodeIPS:
-  - '10.0.1.2'
-  kubernetesClusterId: d-yr8X6Lh0l2
 grafana:
   enabled: true
   service:
@@ -33,5 +29,4 @@ EOF
 helm repo add deepflow https://deepflowio.github.io/deepflow
 helm repo update deepflow # use `helm repo update` when helm < 3.7.0
 helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.4.9 -f values-custom.yaml
-
 #helm upgrade --install deepflow -n monitoring deepflow/deepflow --create-namespace --version 6.2.6 -f values-custom.yaml
