009623c133d113e1420ccd45d7df04506355fc2e
Haitao Pan <manbuzhe2009@qq.com>
2024-12-24 10:27:40 +0000

    add setup-deepflow-server scripts

--- scripts/global-monitor/setup-deepflow-server-ee-all-in-one.sh
+++ scripts/global-monitor/setup-deepflow-server-ee-all-in-one.sh
@@ -6,8 +6,8 @@ curl -sfL https://get.k3s.io | sudo sh -s - --disable=traefik,servicelb         
         --kube-apiserver-arg service-node-port-range=0-50000
 
 sudo mkdir -pv ~/.kube/
-sudo chown -R ubuntu:ubuntu ~/.kube/
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown -R ubuntu:ubuntu ~/.kube/
 
 sudo snap install helm --classic
 
@@ -16,3 +16,13 @@ k8s_node=`sudo kubectl  get nodes | awk 'NR>1{print $1}'`
 sudo kubectl label node $k8s_node master_controller=enable
 sudo kubectl label node $k8s_node tsdb=enable
 sudo kubectl label node $k8s_node dfdb=enable
+
+sudo kubectl create ns deepflow || true
+sudo kubectl create secret docker-registry deepflow-registry-secret \
+  --docker-server=global-images.onwalk.net \
+  --docker-username=admin 		   \
+  --docker-password=Harbor12345 	   \
+  --docker-email=manbuzhe2009@qq.com 	   \
+  -n deepflow
+
+/usr/local/deepflow/bin/deepflow-deploy -i
