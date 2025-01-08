6ab57e790d2fe5f8204a4bc0c1ec5b179f1148a9
Haitao Pan <manbuzhe2009@qq.com>
2025-01-08 05:17:13 +0000

    scripts/deepflow-registry/setup-registry.sh: update

--- scripts/deepflow-registry/setup-registry.sh
+++ scripts/deepflow-registry/setup-registry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #https://github.com/containerd/nerdctl/releases/download/v2.0.2/nerdctl-2.0.2-linux-amd64.tar.gz
+#https://github.com/containerd/nerdctl/releases/download/v2.0.2/nerdctl-full-2.0.2-linux-amd64.tar.gz
+#wget https://github.com/containernetworking/plugins/releases/download/v1.6.2/cni-plugins-linux-amd64-v1.6.2.tgz
 sudo mkdir -pv /opt/deepflow-registry/config/
 sudo cp compose.yaml /opt/deepflow-registry/config/compose.yaml
 sudo cp deepflow-registry.yaml /opt/deepflow-registry/config/deepflow-registry.yaml
@@ -8,3 +10,4 @@ sudo cp deepflow-registry.yaml /opt/deepflow-registry/config/deepflow-registry.y
 sudo CONTAINERD_ADDRESS=/run/k3s/containerd/containerd.sock nerdctl --namespace k8s.io load -i /usr/local/deepflow/registry.tar
 sudo CONTAINERD_ADDRESS=/run/k3s/containerd/containerd.sock nerdctl --namespace k8s.io compose -f /opt/deepflow-registry/config/compose.yaml down
 sudo CONTAINERD_ADDRESS=/run/k3s/containerd/containerd.sock nerdctl --namespace k8s.io compose -f /opt/deepflow-registry/config/compose.yaml up -d
+sudo echo "127.0.0.1 local_registry.onwalk.net" >> /etc/hosts
