19c2bdf8e805960da11a3c8e4f2b4ecfc2e0384a
Haitao Pan <manbuzhe2009@qq.com>
2025-01-08 05:35:25 +0000

    add k3s-cluster scripts

---
+++ scripts/k3s-cluster/cn-k3s-all-in-one.sh
@@ -0,0 +1,17 @@
+sudo mkdir -pv /opt/rancher/k3s
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh \
+	| INSTALL_K3S_MIRROR=cn             \
+	INSTALL_K3S_SKIP_SELINUX_RPM=true   \
+	sh -s -                             \
+	--data-dir=/opt/rancher/k3s         \
+	--kube-apiserver-arg service-node-port-range=0-50000          \
+	--system-default-registry "registry.cn-hangzhou.aliyuncs.com" \
+	--disable=traefik,servicelb
+#curl -sfL https://get.k3s.io | sh -s - --disable=traefik,servicelb                                   \
+#        --data-dir=/opt/rancher/k3s                                                                  \
+#        --kube-apiserver-arg service-node-port-range=0-50000
+
+sudo mkdir -pv ~/.kube/
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+sudo snap install helm --classic
